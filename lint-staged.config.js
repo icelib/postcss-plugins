f2042ecdaadd8ce90a124cac81b3a92d6f28f280
ice breaker <1324318532@qq.com>
2026-08-31 21:30:07 +0800

    chore: migrate workspace tooling to repoctl 5.4.4 (#46)

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,11 +1,3 @@
-export default {
-  '*.{js,jsx,mjs,ts,tsx,mts,vue}': [
-    'eslint --fix --no-warn-ignored',
-  ],
-  '*.{json,md,mdx,css,html,yml,yaml,scss}': [
-    // 'prettier --with-node-modules --ignore-path .prettierignore --write',
-    'eslint --fix --no-warn-ignored',
-  ],
-  // for rust
-  // '*.rs': ['cargo fmt --'],
-}
+import { defineLintStagedConfig } from 'repoctl/tooling'
+
+export default await defineLintStagedConfig()
